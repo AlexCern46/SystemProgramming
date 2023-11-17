e1df911f282a0bb241b40e98441e0096dc995a9a
sasha <sasha.chernyak.04@mail.ru>
2023-11-17 11:07:13 +0300

    rework

--- lab3.py
+++ lab3.py
@@ -40,18 +40,12 @@ def num_to_x(string):
     return new_str
 
 
-def critical_region(process_id):
-    if process_id == 0:
-        time.sleep(2)
-    else:
-        time.sleep(2)
+def critical_region():
+    time.sleep(2)
 
 
-def noncritical_region(process_id):
-    if process_id == 0:
-        time.sleep(1)
-    else:
-        time.sleep(1)
+def noncritical_region():
+    time.sleep(1)
 
 
 class View:
@@ -65,15 +59,28 @@ class View:
 
         self.process = process
 
+        self.label1 = tk.Label(self.root, text="Enter the value for process 1", font=("Arial", 12))
+        self.label1.pack()
+
+        self.entry1 = tk.Entry(self.root, font=("Arial", 12))
+        self.entry1.pack()
+
+        self.label2 = tk.Label(self.root, text="Enter the value for process 2", font=("Arial", 12))
+        self.label2.pack()
+
+        self.entry2 = tk.Entry(self.root, font=("Arial", 12))
+        self.entry2.pack()
+
         self.label_process0 = tk.Label(self.root, text="Process 0: Not in critical region", font=("Arial", 12))
+        self.label_process0.pack()
+
         self.label_process1 = tk.Label(self.root, text="Process 1: Not in critical region", font=("Arial", 12))
+        self.label_process1.pack()
 
         self.start_button = tk.Button(self.root, text="Start Simulation", font=("Arial", 12), command=self.start_simulation)
-        self.stop_button = tk.Button(self.root, text="Stop Simulation", font=("Arial", 12), command=self.stop_simulation)
-
-        self.label_process0.pack()
-        self.label_process1.pack()
         self.start_button.pack()
+
+        self.stop_button = tk.Button(self.root, text="Stop Simulation", font=("Arial", 12), command=self.stop_simulation)
         self.stop_button.pack()
 
     def start_simulation(self):
@@ -86,11 +93,11 @@ class View:
         global running
         running = False
 
-    def update_labels(self):
+    def update_labels(self, value):
         self.label_process0.config(
-            text=f"Process 0: {'In critical region' if self.process0_in_critical else 'Not in critical region'}")
+            text=f"Process 0: {f'In critical region: {value}' if self.process0_in_critical else 'Not in critical region'}")
         self.label_process1.config(
-            text=f"Process 1: {'In critical region' if self.process1_in_critical else 'Not in critical region'}")
+            text=f"Process 1: {f'In critical region: {value}' if self.process1_in_critical else 'Not in critical region'}")
 
     def run(self):
         self.root.mainloop()
@@ -111,24 +118,24 @@ class StrictAlternation:
             while self.view.turn != 0:
                 pass
             self.view.process0_in_critical = True
-            self.view.update_labels()
-            critical_region(0)
+            self.view.update_labels(square(int(self.view.entry1.get())))
+            critical_region()
             self.view.turn = 1
             self.view.process0_in_critical = False
-            self.view.update_labels()
-            noncritical_region(0)
+            self.view.update_labels(None)
+            noncritical_region()
 
     def process1(self):
         while running:
             while self.view.turn != 1:
                 pass
             self.view.process1_in_critical = True
-            self.view.update_labels()
-            critical_region(1)
+            self.view.update_labels(get_ascii(self.view.entry2.get()[0]))
+            critical_region()
             self.view.turn = 0
             self.view.process1_in_critical = False
-            self.view.update_labels()
-            noncritical_region(1)
+            self.view.update_labels(None)
+            noncritical_region()
 
 
 class Peterson:
@@ -139,33 +146,33 @@ class Peterson:
         self.turn = 0
         self.interested = [False for _ in range(self.N)]
 
-        self.process0_thread = threading.Thread(target=self.process1)
-        self.process1_thread = threading.Thread(target=self.process2)
+        self.process0_thread = threading.Thread(target=self.process0)
+        self.process1_thread = threading.Thread(target=self.process1)
 
         self.view = View(self)
         self.view.run()
 
-    def process1(self):
+    def process0(self):
         while running:
             self.enter_region(0)
             self.view.process0_in_critical = True
-            self.view.update_labels()
-            critical_region(0)
+            self.view.update_labels(square(int(self.view.entry1.get())))
+            critical_region()
             self.leave_region(0)
             self.view.process0_in_critical = False
-            self.view.update_labels()
-            noncritical_region(0)
+            self.view.update_labels(None)
+            noncritical_region()
 
-    def process2(self):
+    def process1(self):
         while running:
             self.enter_region(1)
             self.view.process1_in_critical = True
-            self.view.update_labels()
-            critical_region(1)
+            self.view.update_labels(get_ascii(self.view.entry2.get()[0]))
+            critical_region()
             self.leave_region(1)
             self.view.process1_in_critical = False
-            self.view.update_labels()
-            noncritical_region(1)
+            self.view.update_labels(None)
+            noncritical_region()
 
     def enter_region(self, process):
         other = 1 - process
@@ -179,5 +186,5 @@ class Peterson:
 
 
 def main():
-    # simulator = StrictAlternation()
-    simulator = Peterson()
+    simulator = StrictAlternation()
+    # simulator = Peterson()
